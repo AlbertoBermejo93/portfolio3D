13531c501cee2024c08fbfccf447442fb819dfe9
Alberto Bermejo <79146441+AlbertoBermejo93@users.noreply.github.com>
2023-04-14 15:42:07 +0200

    hero component correction

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,70 +5,112 @@ import Navbar from './Navbar'
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-  display:flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content:space-between;
-`
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    height: 200vh;
+  }
+`;
+
 const Container = styled.div`
-  height: 100vh;
+  height: 100%;
   scroll-snap-align: center;
   width: 1400px;
   display: flex;
-  justify-content; space-between;
-`
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
 const Left = styled.div`
   flex: 2;
   display: flex;
   flex-direction: column;
-  jusitfy-content: center;
+  justify-content: center;
   gap: 20px;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    align-items: center;
+  }
+`;
 
-`
 const Title = styled.h1`
   font-size: 74px;
-`
+  @media only screen and (max-width: 768px) {
+    text-align: center;
+  }
+`;
+
 const WhatWeDo = styled.div`
-  display:flex;
-  align-items:center;
-  gap:10px;
-`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
 const Line = styled.img`
   height: 5px;
-`
+`;
+
 const Subtitle = styled.h2`
-  color: pink;
-`
+  color: #da4ea2;
+`;
+
 const Desc = styled.p`
-  color:graylight;
   font-size: 24px;
-`
+  color: lightgray;
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    text-align: center;
+  }
+`;
+
 const Button = styled.button`
-  bakcground-color: #da4ea2;
-  color:white;
-  font-weight:500;
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
   width: 100px;
   padding: 10px;
   border: none;
   border-radius: 5px;
   cursor: pointer;
-`
+`;
 
 const Right = styled.div`
-  flex:3;
+  flex: 3;
   position: relative;
+  @media only screen and (max-width: 768px) {
+    flex: 1;
+    width: 100%;
+  }
 `;
+
 const Img = styled.img`
-  widht: 800px;
+  width: 800px;
   height: 600px;
   object-fit: contain;
   position: absolute;
   top: 0;
   bottom: 0;
   left: 0;
-  right:0;
+  right: 0;
   margin: auto;
-`
+  animation: animate 2s infinite ease alternate;
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+  @keyframes animate {
+    to {
+      transform: translateY(20px);
+    }
+  }
+`;
 
 const Hero = () => {
   return (
