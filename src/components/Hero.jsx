21a4f0911905112c0395416b7f76b251d7fb7d1e
Alberto Bermejo <79146441+AlbertoBermejo93@users.noreply.github.com>
2023-04-14 10:42:47 +0200

    main page components

---
+++ src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from './Navbar'
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display:flex;
+  flex-direction: column;
+  align-item: center;
+  justify-content:space-between;
+`
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content; space-between;
+`
+const Left = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content; space-between;
+`
+const Right = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1400px;
+  display: flex;
+  justify-content; space-between;
+`
+
+const Hero = () => {
+  return (
+    <Section>
+      <Navbar/>
+      <Container>
+        <Left>
+          <Title>Think.Make.Solve.</Title>
+          <WhatWeDo>
+            <Line src="./img/line.png"/>
+            <Subtitle>What we Do</Subtitle>
+          </WhatWeDo>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  )
+}
+
+export default Hero
